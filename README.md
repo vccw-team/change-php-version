1e157e15d50eccad5fdd4dd32e9978ceaa6e123f
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-21 22:15:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Change PHP version for the VCCW
+
+It is a simple shell script that installs any version of PHP you need.
+
+## How to use
+
+Pleace the `provision-post.sh` into your VCCW directory. You can choose PHP version 5.6 or 7.2 or so.
+
+```
+#! /usr/bin/env bash
+
+set -ex
+
+curl https://raw.githubusercontent.com/vccw-team/change-php-version/master/run.sh | bash -s -- 5.6
+```
+
+Then run:
+
+```
+$ vagrant up
+```
+
+Or
+
+```
+$ vagrant provision
+```
---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+PHP_VERSION=${1-"7.0"}
+
+sudo apt update -y
+
+sudo apt install \
+php${PHP_VERSION} \
+php${PHP_VERSION}-cli \
+php${PHP_VERSION}-common \
+php${PHP_VERSION}-curl \
+php${PHP_VERSION}-dev \
+php${PHP_VERSION}-gd \
+php${PHP_VERSION}-json \
+php${PHP_VERSION}-mbstring \
+php${PHP_VERSION}-mysql \
+php${PHP_VERSION}-opcache \
+php${PHP_VERSION}-readline \
+php${PHP_VERSION}-xml \
+php${PHP_VERSION}-zip -y
+
+sudo /usr/sbin/a2dismod php*
+sudo /usr/sbin/a2enmod php${PHP_VERSION}
+
+sudo service apache2 restart 
+sudo update-alternatives --set php /usr/bin/php${PHP_VERSION}
