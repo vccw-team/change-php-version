4ef2433d8be3de1b21190eb3a4cd37c4521ffd0d
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-22 01:41:20 +0900

    change php version

--- run.sh
+++ run.sh
@@ -4,24 +4,22 @@ set -ex
 
 PHP_VERSION=${1-"7.0"}
 
-if [[ "7.0" -ne "$PHP_VERSION" ]]; then
-  sudo apt update -y
+sudo apt update -y
 
-  sudo apt install \
-  php${PHP_VERSION} \
-  php${PHP_VERSION}-cli \
-  php${PHP_VERSION}-common \
-  php${PHP_VERSION}-curl \
-  php${PHP_VERSION}-dev \
-  php${PHP_VERSION}-gd \
-  php${PHP_VERSION}-json \
-  php${PHP_VERSION}-mbstring \
-  php${PHP_VERSION}-mysql \
-  php${PHP_VERSION}-opcache \
-  php${PHP_VERSION}-readline \
-  php${PHP_VERSION}-xml \
-  php${PHP_VERSION}-zip -y
-fi
+sudo apt install \
+php${PHP_VERSION} \
+php${PHP_VERSION}-cli \
+php${PHP_VERSION}-common \
+php${PHP_VERSION}-curl \
+php${PHP_VERSION}-dev \
+php${PHP_VERSION}-gd \
+php${PHP_VERSION}-json \
+php${PHP_VERSION}-mbstring \
+php${PHP_VERSION}-mysql \
+php${PHP_VERSION}-opcache \
+php${PHP_VERSION}-readline \
+php${PHP_VERSION}-xml \
+php${PHP_VERSION}-zip -y
 
 sudo /usr/sbin/a2dismod php*
 sudo /usr/sbin/a2enmod php${PHP_VERSION}
